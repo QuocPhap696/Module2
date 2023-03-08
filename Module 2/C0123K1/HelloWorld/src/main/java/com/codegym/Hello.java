da1a7e12a8f5ec790fc861558f84af6bcf5865d5
Quoc Phap <QuocPhap@Phap-Mac-M1.local>
2023-03-08 15:15:29 +0700

    modul2

---
+++ Module 2/C0123K1/HelloWorld/src/main/java/com/codegym/Hello.java
@@ -0,0 +1,18 @@
+package com.codegym;
+
+import java.awt.*;
+import java.util.Date;
+import java.util.Scanner;
+
+public class Hello {
+    public static void main(String[] args) {
+
+        Date now = new Date();
+        System.out.println(now);
+        }
+    }
+
+
+
+
+
