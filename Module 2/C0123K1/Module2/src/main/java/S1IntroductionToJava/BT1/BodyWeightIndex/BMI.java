8892b66d9f152e6c292fdc0b59af3b1483364d08
Quoc Phap <QuocPhap@Phap-Mac-M1.local>
2023-03-09 14:28:08 +0700

    Nhập tên hiển thị lời chào

--- Module 2/C0123K1/Module2/src/main/java/S1IntroductionToJava/BT1/BodyWeightIndex/BMI.java
+++ Module 2/C0123K1/Module2/src/main/java/S1IntroductionToJava/BT1/BodyWeightIndex/BMI.java
@@ -24,8 +24,6 @@ public class BMI {
         } else {
             System.out.printf("%-20.2f%s", bmi, "Obese");
         }
-
-
     }
 }
 
---
+++ Module 2/C0123K1/Module2/src/main/java/S1IntroductionToJava/BT1/ShowGreetings/Hienthiloichao.java
@@ -0,0 +1,13 @@
+package S1IntroductionToJava.BT1.ShowGreetings;
+
+import java.util.Scanner;
+public class Hienthiloichao {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Nhập tên vô ba");
+
+        String name = sc.nextLine();
+        System.out.println("Tên của bạn : " + name);
+    }
+    }
+
