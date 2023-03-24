0e31e5e75a92603530b8219440aa40e033960670
QuocPhap696 <Dquocphap@gmail.com>
2023-03-24 16:25:20 +0700

    Linked_List

--- src/main/java/S1IntroductionToJava/BT1/BTthayQuang/TestLiked_List/MyLinkedList.java
+++ src/main/java/S1IntroductionToJava/BT1/BTthayQuang/TestLiked_List/MyLinkedList.java
@@ -4,50 +4,51 @@ public class MyLinkedList {
     private Node head;
     private int numNodes;
 
-    public MyLinkedList (Object data){
+    public MyLinkedList(Object data) {
         head = new Node(data);
         numNodes++;
     }
-    public class Node {
-        private Node next;
-        private Object data;
-
-        public Node(Object data) {
-            this.data = data;
-        }
-        public Object getData() {
-            return this.data;
-        }
-    }
-    // cài đặt phương thức add
-    public void add (int index, Object data) {
-        Node temp = head;
-        Node holder;
-
-        for (int i = 0; i < index - 1 && temp.next != null; i++) {
-            temp = temp.next;
-        }
-        holder = temp.next;
-        temp.next = new Node(data);
-        temp.next.next = holder;
-        numNodes++;
-    }
-    // cài đặt phương thức addFirst
+//    thêm phương thức addFirst(thêm đối tượng vào vị trí đầu trong danh sách)
     public void addFirst(Object data) {
         Node temp = head;
         head = new Node(data);
         head.next = temp;
         numNodes++;
     }
-    // cài đặt phương thức get (lấy phần tử ở vị trí index từ danh sách)
-    public Node get (int index) {
+    //
+    private class Node {
+        private Node next;
+        private Object data;
+
+     public Node (Object data){
+        this.data = data;
+     }
+     // cài đặt phương thức get() (lấy phần tử ở vị trí index từ danh sách)
+     public Object getData() {
+         return this.data;
+     }
+    }
+    //cài đặt phương thức add
+    public void add (int index, Object data) {
+        Node temp = head;
+        Node holder;
+
+        for (int i = 0; i < index -1 && temp.next != null; i++) {
+            temp = temp.next;
+        }
+        holder = temp.next;
+            temp.next = new Node(data);
+            temp.next.next = holder;
+            numNodes++;
+    }
+    //cài đặt phương thức get() (lấy phần ử ở vị trí index)
+        public Node get(int index) {
         Node temp = head;
         for (int i = 0; i < index; i++) {
             temp = temp.next;
+        } return temp;
         }
-        return  temp;
-    }
-    // cài đặt phương thức printList (hiển thị phần tử trong danh sách)
+    // cài đặt phương thức printList (hiển thị các phần tử trong danh sách)
     public void printList() {
         Node temp = head;
         while (temp != null) {
@@ -55,5 +56,4 @@ public class MyLinkedList {
             temp = temp.next;
         }
     }
-
 }
