a8315e3f569f62fb6dedcde42a3f223bce80abdd
QuocPhap696 <Dquocphap@gmail.com>
2023-04-03 16:52:02 +0700

    LinkedHashMap

---
+++ src/main/java/S1IntroductionToJava/BT1/HashMap_LinkedHashMap_Treemap/TestMap.java
@@ -0,0 +1,31 @@
+package S1IntroductionToJava.BT1.HashMap_LinkedHashMap_Treemap;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class TestMap {
+    public static void main(String[] args) {
+        Map<String, Integer> hashMap = new HashMap<>();
+        hashMap.put("Smith", 30);
+        hashMap.put("Anderson", 31);
+        hashMap.put("Lewis", 29);
+        hashMap.put("Cook", 29);
+        System.out.println("Display entries in HashMap");
+        System.out.println(hashMap + "\n");
+
+        //tạo Treemap để lưu key theo thứ tự giảm dần
+    Map<String, Integer> treeMap = new TreeMap<>(hashMap);
+        System.out.println("Display entries in ascending oder of key");
+        System.out.println(treeMap);
+
+        //tạo LinkedHashMap để lưu danh sách sinh viên  và hiển thị dữ liệu
+        Map<String, Integer> linkedHashMap = new LinkedHashMap<>(16, 0.75f,true);
+        linkedHashMap.put("Smith", 30);
+        linkedHashMap.put("Anderson", 31);
+        linkedHashMap.put("Lewis", 29);
+        linkedHashMap.put("Cook", 29);
+        System.out.println("\nThe age of " + "Lewis is " + linkedHashMap.get("Lewis"));
+    }
+}
